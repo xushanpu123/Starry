c58a8c2e612514f3f2f5f33b3be2f471181eccd8
yufeng <321353225@qq.com>
2024-01-29 15:02:18 +0800

    Merge remote-tracking branch 'monolithic/x86_64' into HEAD

--- ulib/axstarry/syscall_entry/src/syscall.rs
+++ ulib/axstarry/syscall_entry/src/syscall.rs
@@ -9,7 +9,7 @@ pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
         #[cfg(feature = "syscall_net")]
         {
             if let Ok(net_syscall_id) = syscall_net::NetSyscallId::try_from(syscall_id) {
-                axlog::info!("syscall: {:?}", net_syscall_id);
+                axlog::info!("[syscall] id = {:#?}, args = {:?}, entry", net_syscall_id, args);
                 break syscall_net::net_syscall(net_syscall_id, args);
             }
         }
@@ -17,7 +17,7 @@ pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
         #[cfg(feature = "syscall_mem")]
         {
             if let Ok(mem_syscall_id) = syscall_mem::MemSyscallId::try_from(syscall_id) {
-                axlog::info!("syscall: {:?}", mem_syscall_id);
+                axlog::info!("[syscall] id = {:#?}, args = {:?}, entry", mem_syscall_id, args);
                 break syscall_mem::mem_syscall(mem_syscall_id, args);
             }
         }
@@ -25,7 +25,7 @@ pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
         #[cfg(feature = "syscall_fs")]
         {
             if let Ok(fs_syscall_id) = syscall_fs::FsSyscallId::try_from(syscall_id) {
-                axlog::info!("syscall: {:?}", fs_syscall_id);
+                axlog::info!("[syscall] id = {:#?}, args = {:?}, entry", fs_syscall_id, args);
                 break syscall_fs::fs_syscall(fs_syscall_id, args);
             }
         }
@@ -34,7 +34,7 @@ pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
 
         {
             if let Ok(task_syscall_id) = syscall_task::TaskSyscallId::try_from(syscall_id) {
-                axlog::info!("syscall: {:?}", task_syscall_id);
+                axlog::info!("[syscall] id = {:#?}, args = {:?}, entry", task_syscall_id, args);
                 break syscall_task::task_syscall(task_syscall_id, args);
             }
         }
